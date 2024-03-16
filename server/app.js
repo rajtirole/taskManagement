3d2cdc269e88404b34bd4308bb680e201703bee4
rajtirole <rtech23456@gmail.com>
2024-03-16 10:26:50 +0530

    task management project completed

---
+++ server/app.js
@@ -0,0 +1,94 @@
+import { config } from 'dotenv'
+import express from 'express'
+import multer from 'multer'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import erroMiddleware from './middlewares/error.middleware.js'
+import userRoutes from './routes/user.routes.js'
+import dashboardRoutes from './routes/dashboardRoutes.js'
+import taskRoutes from './routes/usertask.js'
+
+import  course from './routes/course.js'
+import formidable from  'express-formidable';
+// const cors = require('cors');
+// const allowedOrigins = ["http://TheCodebuzz.com", "http://TheCodebuzz.test.com","http://localhost:3000/","http://localhost:300","http://localhost:5400","http://localhost:300","http://localhost/:10"];
+const allowedOrigins= ["http://localhost:3000","http://localhost:300"];
+
+config();
+// const PORT=process.env.PORT;
+// console.log(PORT);
+const app = express();
+// app.use(cors())
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     }
+// }));
+// const upload=multer();
+// app.use(upload.none());  
+// app.use(express.json())
+// const formidable = require('express-formidable');
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:3000",
+    })
+);
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }))    
+app.use(bodyParser.urlencoded({ extended: true })) 
+app.use(cookieParser())
+app.use(morgan('dev')) 
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL],
+//     credentials:true
+// }))
+// console.log(req.body);
+
+
+
+
+
+
+
+app.get('/ping',(req,res)=>{
+    res.send('pong server');
+    
+})
+
+app.use('/api/v1/user',userRoutes)
+app.use('/api/v1/dashboard',dashboardRoutes)
+app.use('/api/v1/taskroutes',taskRoutes)
+// app.use('/api/v1/taskroutes',()=>{
+//     console.log('taslkfjsldkfjlk');
+// })
+// app.use('/api/v1/dashboard',()=>{
+//     console.log('get dashboard routes');
+// })
+// app.use('/api/v1/course',course)
+// app.use('/api/v1/user',()=>{
+//     console.log("fjdkjflk");
+// })
+
+
+
+
+
+
+app.all('*',(req,res)=>{
+    res.status(500).send('page not have any')
+    
+})
+app.use(erroMiddleware)
+export default app;
---
+++ server/routes/user.routes.js
@@ -0,0 +1,28 @@
+import{ Router} from 'express'
+import upload from '../middlewares/multer.middleware.js';
+import auth from '../middlewares/user.auth.js'
+import{ register,login,logout,forgot,reset,changePassword,updateProfile,getdata} from '../controller/user.controller.js'
+// import { reset } from 'nodemon';
+const router=Router();
+// console.log("rotuter");
+
+// router.post('/register',() => {
+//     console.log("reffkjlsjflaks");
+// })
+router.get('/register',register)
+router.post('/register',register)
+router.post('/login',login)
+router.get('/logout',logout)
+router.get('/getdata',auth,getdata)
+// router.get('/me',auth,getProfile)
+router.post('/forgot',forgot)
+// router.post('/forgot',(req,res) =>{
+// console.log('flakjsdlkfjasldkfjsdlkfjsd');
+// console.log(req.body);
+// })
+// router.post('/reset',reset)
+// router.post('/reset',reset)
+router.post('/reset',reset)
+router.post('/changePassword',auth,changePassword)
+router.post('/updateProfile',auth,upload.single('avatar'),updateProfile)
+    export default router;
